ef9a01794b7135d3a9722efb7ab38841af4618e7
Aristotelis-crtl <jabin.julian@gmail.com>
2022-01-18 16:39:25 +0100

    refacto logo and add it to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import useBreakpoint from "../hooks/useBreakpoint";
 import RandomCity from "./RandomCity";
+import pic from "../img/logo.png";
 const Navbar = () => {
   const bp = useBreakpoint();
   const location = useLocation();
 
   return (
     <>
-      <nav className="relative select-none bg-blueperso flex items-stretch py-2">
+      <nav className="relative select-none bg-blueperso flex items-stretch py-2 items-center">
         <div className="flex flex-no-shrink items-stretch h-12">
+          <img src={pic} alt="logo" className="ml-4" />
+
           <Link
             to="/"
             className={`flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal no-underline flex items-center hover:text-yellowperso ${
